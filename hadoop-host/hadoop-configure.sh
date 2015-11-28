f4473714bbbca23d4a4d84e3049af63dfd5b622a
Ihor Banadiga <banadiga@gmail.com>
2015-11-28 11:54:55 +0200

    memory added, cpus added, $HADOOP_OPTS fixed

--- hadoop-host/hadoop-configure.sh
+++ hadoop-host/hadoop-configure.sh
@@ -10,7 +10,7 @@ export HADOOP_COMMON_HOME=$HADOOP_HOME
 export HADOOP_HDFS_HOME=$HADOOP_HOME
 export YARN_HOME=$HADOOP_HOME
 export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native
-export HADOOP_OPTS="-Djava.library.path=$HADOOP_HOME/lib"
+export HADOOP_OPTS="$HADOOP_OPTS -Djava.library.path=$HADOOP_HOME/lib/native"
 
 ## Configuring SSH
 ssh-keygen -t rsa -P "" -f $HOME/.ssh/id_rsa
@@ -33,7 +33,7 @@ export HADOOP_COMMON_HOME=$HADOOP_HOME
 export HADOOP_HDFS_HOME=$HADOOP_HOME
 export YARN_HOME=$HADOOP_HOME
 export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native
-export HADOOP_OPTS="-Djava.library.path=$HADOOP_HOME/lib"
+export HADOOP_OPTS="$HADOOP_OPTS -Djava.library.path=$HADOOP_HOME/lib/native"
 # -- HADOOP ENVIRONMENT VARIABLES END -- #
 EOT
 
